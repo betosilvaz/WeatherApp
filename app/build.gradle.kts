29efbed046aa32452a7ea4b417313baa16b6a1b9
Gilberto Silva <gilbertozn527@gmail.com>
2026-05-15 14:04:00 -0300

    Pratica 2 - Parte 1
    Obs: corrige também mistyping da parte 0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,6 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     implementation("androidx.navigation:navigation-compose:2.9.8")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.9")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
     implementation("androidx.compose.material:material-icons-extended")
 }
--- app/src/main/java/com/example/weatherapp2/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp2/MainActivity.kt
@@ -1,29 +1,15 @@
 package com.example.weatherapp2
 
-import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.LocalActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.weatherapp2.ui.HomePage
 import com.example.weatherapp2.ui.theme.WeatherApp2Theme
-import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,24 +27,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun HomePage(modifier: Modifier = Modifier) {
-    val activity = LocalActivity.current as Activity
-    Column(
-        modifier = modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Boas Vindas!")
-        Button(
-            onClick = {
-                Toast.makeText(activity, "Saindo...", Toast.LENGTH_LONG).show()
-                      activity.finish()
-                      },
-            modifier = Modifier.fillMaxWidth(fraction = 0.9f).padding(top = 24.dp),
-        ) {
-            Text("Sair")
-        }
-    }
-
-}
