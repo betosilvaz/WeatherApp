8b29f9f94e41ed136381d7262a7c46bdd9e556d1
Gilberto Silva <gilbertozn527@gmail.com>
2026-06-05 16:48:08 -0300

    pratica 6 - parte 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
